426e2a06c22f5740e8c8a289560e24b935b30f32
barcode880 <zwonyoung33@gmail.com>
2025-09-09 10:23:11 +0900

    add httpd setting comment

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,4 @@
+<h1>hello</h1>
 <?php
 $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', getenv('DB_HOST'), getenv('DB_NAME'));
 $user = getenv('DB_USER');
@@ -10,7 +11,7 @@ try {
     ]);
     $rows = $pdo->query('SELECT NOW() AS server_time')->fetchAll();
     echo "<h1>Apache + PHP-FPM + MariaDB (Docker)</h1>";
-    echo "<p>DB 연결 OK. 서버시각: <b>{$rows[0]['server_time']}</b></p>";
+    echo "<p>DB 연결 OK. 서버시각입니다: <b>{$rows[0]['server_time']}</b></p>";
 } catch (Throwable $e) {
     http_response_code(500);
     echo "<h1>DB 연결 실패</h1><pre>{$e->getMessage()}</pre>";
